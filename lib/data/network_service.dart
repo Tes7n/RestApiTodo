6547fe16d5a6d04fc07c147abaf6b711f76a5219
Text('Tushar') <tesvn7@gmail.com>
2021-10-28 15:18:07 +0530

    backup2

--- lib/data/network_service.dart
+++ lib/data/network_service.dart
@@ -2,11 +2,12 @@ import 'dart:convert';
 import 'package:http/http.dart';
 
 class NetworkService {
-  final baseUrl = "http://127.0.0.1:3000";
+  final baseUrl = "http://192.168.29.231:3000";
   Future<List<dynamic>> fetchTodos() async {
     try {
       final response = await get(Uri.parse(baseUrl + "/todos"));
       print(response.body);
+      print("hello");
       return jsonDecode(response.body) as List;
     } catch (e) {
       print(e);
